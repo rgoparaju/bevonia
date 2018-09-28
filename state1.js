4279e3f7a39a04f27422a85ebb11c69ad27122f2
rgoparaju <ravi.goparaju@utexas.edu>
2018-09-28 12:02:33 -0500

    test color change

--- state1.js
+++ state1.js
@@ -2,7 +2,7 @@ var demo = {}
 demo.state1 = function(){}
 demo.state1.prototype = {
     preload: function(){
-        game.stage.backgroundColor = '#b7b5b5'
+        game.stage.backgroundColor = '#71f442'
     },
     create: function(){},
     update: function(){}
