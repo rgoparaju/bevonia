d85d173a865fe5f3cd588a17cd73a87e6e2ff3f9
aidanlokeeffe <aidanlokeeffe@.utexas.edu>
2018-10-06 23:37:13 -0500

    Update bevonia 2.0/state1.js
    readabilitizing, more art work, NEED HELP GETTING BACKGROUND to display

--- bevonia 2.0/state1.js
+++ bevonia 2.0/state1.js
@@ -6,18 +6,23 @@ demo.state1.prototype = {
         game.load.tilemap("level1", "assets/tilemaps/level1.json", null, Phaser.Tilemap.TILED_JSON);
         game.load.image("brick", "assets/sprites/wall sprite 2.png");
         game.load.image("spikes", "assets/sprites/deathSpikes.png");
+        game.load.image('bg1', 'assets/sprites/test background v2.png');
+        
         // Bevonia
         game.load.spritesheet("bevonia", "assets/sprites/Bevonia.png", 32, 48);
         game.load.spritesheet("armoredBevonia", "assets/sprites/bevoniaArmored.png", 32, 48);
-        game.load.spritesheet("stabBevonia", "assets/sprites/bevoThrust", 48, 48);        
+        game.load.spritesheet("stabBevonia", "assets/sprites/bevoThrust", 48, 48);   
+        
         // Enemies
         game.load.spritesheet("bat", "assets/sprites/bat.png", 32, 32);
         game.load.spritesheet("chest", "assets/sprites/chest.png", 64, 64);
-        game.load.spritesheet("spider", "assets/sprites/spider.png", 48, 48);        
+        game.load.spritesheet("spider", "assets/sprites/spider.png", 48, 48); 
+        
         // Items
         game.load.spritesheet("helmet", "assets/sprites/helmet.png", 32, 48);
         game.load.spritesheet("key", "assets/sprites/key.png", 32, 48);
-        game.load.spritesheet("sword", "assets/sprites/sword.png", 32, 64);        
+        game.load.spritesheet("sword", "assets/sprites/sword.png", 32, 64);   
+        
         // Misc.
         game.load.spritesheet("healthBar", "assets/sprites/healthBar.png", 256, 16);
         game.load.spritesheet("manaBar", "assets/sprites/manaBar.png", 256, 16);
@@ -27,7 +32,43 @@ demo.state1.prototype = {
         
         console.log("state1");
         
-        //Display
+        // Display settings
+        game.world.setBounds(0, 0, 2000, 500);
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        // Add background
+        game.add.sprite(0, 0, "bg1");
+        
+        // Create map
+        var map1 = game.add.tilemap("level1");
+        map.addTilesetImage("brick");
+        map.addTilesetImage("spikes");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     },
     update: function(){}
 }
