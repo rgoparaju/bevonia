e3cabbba479d32925c6798f691c9adf5e90ebec7
aidanlokeeffe <aidanlokeeffe@.utexas.edu>
2018-11-10 17:56:42 -0600

    old additions

--- bevonia_alpha/state3.js
+++ bevonia_alpha/state3.js
@@ -2,7 +2,14 @@ var bevonia
 var map3, platforms3, traps3
 demo.state3 = function(){}
 demo.state3.prototype = {
-    create: function(){
+    preload: function () {
+        game.load.tilemap('level3', 'assets/tilemaps/level3.json', null, Phaser.Tilemap.TILED_JSON)  
+        
+    
+
+        
+    },
+    create: function () {
         var background3 = game.add.sprite(0,0,'bg3')
         
         var map3 = game.add.tilemap("level3");
@@ -10,14 +17,14 @@ demo.state3.prototype = {
         map3.addTilesetImage("deathSpikes");
         
         game.world.setBounds(0, 0, 3200, 1600);
-//        
-//        platforms3 = map3.createLayer("platforms 3");
-//        traps3 = map3.createLayer("traps 3");
-//        
-//        map3.setCollision(1, true, "platforms 3");
-//        map3.setCollision([2, 3, 4, 5, 6, 7, 8], true, "traps 3");
-//        
-//        bevonia = new Bevonia(704,192, 1344)
+        
+        platforms3 = map3.createLayer("platforms 3");
+        traps3 = map3.createLayer("traps 3");
+        
+        map3.setCollision(1, true, "floorV4");
+        map3.setCollision([2, 3, 4, 5, 6, 7, 8], true, "deathSpikes");
+        
+        bevonia = new Bevonia(704,192, 1344)
         
         
     },
