58a08a910fb0a955a424efde0a002e1f3a4d5999
rgoparaju <tejagoparaju@gmail.com>
2018-11-13 22:36:14 -0600

    Update state3.js

--- bevonia_alpha/state3.js
+++ bevonia_alpha/state3.js
@@ -3,31 +3,46 @@ var map3, platforms3, traps3
 demo.state3 = function(){}
 demo.state3.prototype = {
     preload: function () {
-        game.load.tilemap('level3', 'assets/tilemaps/level3.json', null, Phaser.Tilemap.TILED_JSON)  
+        game.load.tilemap('level3', 'assets/tilemaps/level 3 v2.json', null, Phaser.Tilemap.TILED_JSON)  
         
     
 
         
     },
     create: function () {
-        var background3 = game.add.sprite(0,0,'bg3')
+        var background3 = game.add.sprite(0,10,'bg3')
         
         var map3 = game.add.tilemap("level3");
         map3.addTilesetImage("floorV4");
-        map3.addTilesetImage("deathSpikes");
-        
+        map3.addTilesetImage("spikes");
+//        
         game.world.setBounds(0, 0, 3200, 1600);
+//        
+        platforms3 = map3.createLayer("platforms");
+        traps3 = map3.createLayer("traps");
+//        
+        map3.setCollision(1, true, "platforms");
+        map3.setCollision([2, 3, 4, 5, 6, 7, 8], true, "traps");
         
-        platforms3 = map3.createLayer("platforms 3");
-        traps3 = map3.createLayer("traps 3");
-        
-        map3.setCollision(1, true, "floorV4");
-        map3.setCollision([2, 3, 4, 5, 6, 7, 8], true, "deathSpikes");
-        
-        bevonia = new Bevonia(704,192, 1344)
+        bevonia = new Bevonia(608,224, 1632)
         
         
     },
-    update: function(){}
+    update: function(){
+        
+        game.physics.arcade.collide(bevonia.self, platforms3)
+        
+        bevonia.run()
+        bevonia.jump()
+        bevonia.die()
+        bevonia.manageVulnerability()
+        bevonia.stab()
+        bevonia.castAOE()
+        bevonia.castPrecise()
+        
+        if (game.physics.arcade.collide(bevonia.self, traps3)) {
+            bevonia.health -= 1;
+        }
+    }
     
 }
