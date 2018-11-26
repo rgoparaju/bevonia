f300d2f6970a6832faab8582f7d70669aec5e03f
aidanlokeeffe <aidanlokeeffe@.utexas.edu>
2018-11-25 19:54:39 -0600

    enemies in state 3 DONE

--- bevonia_alpha/state3.js
+++ bevonia_alpha/state3.js
@@ -43,11 +43,88 @@ demo.state3.prototype = {
         
         items3 = [door3, chest3, armor3, sword3, key3, health3, mana3, exitKey3];
         
+        skel3_1 = new Skeleton(400, 592, 322, 417, bevonia);
+        skel3_2 = new Skeleton(300, 1424, 127, 387, bevonia);
+        skel3_3 = new Skeleton(1500, 592, 1214, 1604, bevonia);
+        skel3_4 = new Skeleton(1000, 752, 997, 1057, bevonia);
+        skel3_5 = new Skeleton(1200, 912, 1182, 1287, bevonia);
+        skel3_6 = new Skeleton(1600, 784, 1507, 1637, bevonia);
+        skel3_7 = new Skeleton(2300, 912, 2276, 2496, bevonia);
+        skel3_8 = new Skeleton(2000, 272, 1938, 2303, bevonia);
+        skel3_9 = new Skeleton(2600, 272, 2463, 2848, bevonia);
         
+        spider3_1 = new Spider(1608, 1200, 1216, 1424, "y", -1, bevonia);
+        spider3_2 = new Spider(1880, 550, 526, 688, "y", 1, bevonia);
+        spider3_3 = new Spider(2488, 650, 648, 720, "y", 1, bevonia);
+        spider3_4 = new Spider(3080, 1200, 1066, 1328, "y", -1, bevonia);
+        spider3_5 = new Spider(1560, 100, 32, 212, "y", 1, bevonia);
         
+        bat3_1 = new Bat(2467, 1100, bevonia);
+        bat3_2 = new Bat(3120, 275, bevonia);
+        bat3_3 = new Bat(1770, 34, bevonia);
+        bat3_4 = new Bat(1217, 34, bevonia);
+        
+        enemies3 = [skel3_1, skel3_2, skel3_3, skel3_4, skel3_5, skel3_6, skel3_7, skel3_8, skel3_9, spider3_1, spider3_2, spider3_3, spider3_4, spider3_5, bat3_1, bat3_2, bat3_3, bat3_4]
     },
     update: function(){
         game.physics.arcade.collide(bevonia.self, platforms3)
+        bevonia.run()
+        bevonia.jump()
+        bevonia.die()
+        bevonia.manageVulnerability()
+        bevonia.stab()
+        bevonia.castAOE()
+        bevonia.castPrecise()
+        
+        skel3_1
+        skel3_2
+        skel3_3
+        skel3_4
+        skel3_5
+        skel3_6
+        skel3_7
+        skel3_8
+        skel3_9
+        spider3_1
+        spider3_2
+        spider3_3
+        bat3_1
+        bat3_2
+        bat3_3
+        bat3_4
+        
+        game.physics.arcade.collide(skel3_1.self, platforms3);
+        game.physics.arcade.collide(skel3_2.self, platforms3);
+        game.physics.arcade.collide(skel3_3.self, platforms3);
+        game.physics.arcade.collide(skel3_4.self, platforms3);
+        game.physics.arcade.collide(skel3_5.self, platforms3);
+        game.physics.arcade.collide(skel3_6.self, platforms3);
+        game.physics.arcade.collide(skel3_7.self, platforms3);
+        game.physics.arcade.collide(skel3_8.self, platforms3);
+        game.physics.arcade.collide(skel3_9.self, platforms3);
+        game.physics.arcade.collide(bat3_1.self, platforms3);
+        game.physics.arcade.collide(bat3_2.self, platforms3);
+        game.physics.arcade.collide(bat3_3.self, platforms3);
+        game.physics.arcade.collide(bat3_4.self, platforms3);
+        
+        skel3_1.patrol();
+        skel3_2.patrol();
+        skel3_3.patrol();
+        skel3_4.patrol();
+        skel3_5.patrol();
+        skel3_6.patrol();
+        skel3_7.patrol();
+        skel3_8.patrol();
+        skel3_9.patrol();
+        spider3_1.patrol();
+        spider3_2.patrol();
+        spider3_3.patrol();
+        spider3_4.patrol();
+        spider3_5.patrol();
+        bat3_1.watch();
+        bat3_2.watch();
+        bat3_3.watch();
+        bat3_4.watch();
         
         game.physics.arcade.collide(health3.self,platforms3)
         game.physics.arcade.collide(mana3.self,platforms3)
@@ -61,13 +138,96 @@ demo.state3.prototype = {
             }
         }
         
-        bevonia.run()
-        bevonia.jump()
-        bevonia.die()
-        bevonia.manageVulnerability()
-        bevonia.stab()
-        bevonia.castAOE()
-        bevonia.castPrecise()
+        var j; for (j = 0; j < enemies3.length; j++) {
+            if (game.physics.arcade.overlap(bevonia.self, enemies3[j].self) && !enemies3[j].vulnerable) {
+                if (bevonia.stabbing) {
+                    Skeleton.prototype.toString = function(){
+                        return 'Skeleton'
+                    }
+                    
+                    if (enemies3[j].toString() == 'Skeleton'){
+                    
+                    var enemy_distance = 1000;
+                    if (knockedTo == 0){
+                        enemies3[j].self.animations.stop();
+                        knockedTo = (enemies3[j].self.body.x - (enemy_distance*2));
+                    }
+//                    enemies1[j].self.body.velocity.x = -500;
+                    if (enemies3[j].self.body.x <= (knockedTo + enemy_distance/2)){
+                        if (bevonia.self.scale.x == -1){
+//                        enemies1[j].self.body.velocity.x = -500;
+                        enemies3[j].self.body.velocity.y = -200;
+                        console.log("FUCK")
+                        }
+                        else if (bevonia.self.scale.x == 1){
+                            console.log("UM");
+//                            enemies1[j].self.body.velocity.x = 500;
+                            enemies3[j].self.body.velocity.y = -200;
+                        }
+//                        game.time.events.add(20, enemies1[j].self.body.velocity.y = -10, this);
+                    }
+                    else{
+                        if (bevonia.self.scale.x == -1){
+//                        enemies1[j].self.body.velocity.x = -500;
+                        enemies3[j].self.body.velocity.y = -200;
+                        console.log("SHIT")
+                        }
+                        else if (bevonia.self.scale.x == 1){
+                            console.log("UM SHIT");
+//                            enemies1[j].self.body.velocity.x = 500;
+                            enemies3[j].self.body.velocity.y = -200;
+                        }
+                    }
+                    if (enemies3[j].self.body.x <= knockedTo){
+                        console.log("BITCH")
+                        enemies3[j].frame = 1;
+                        knockedTo = 0;
+                        knockback = false;
+                    }
+//                     enemies1[j].self.body.velocity.x = -250   
+                    }
+                    
+                    enemies3[j].hitCount += 1;
+                    enemies3[j].vulnerable = true;
+                    enemies3[j].die();
+                    enemies3[j].invincibilityTimer = game.time.now + 500;
+                    
+                }
+                else if (bevonia.vulnerable) {
+                    bevonia.health -= bevonia.damageFactor;
+                    bevonia.self.animations.stop();
+                var distance = 1000;
+                if (knockedTo == 0){
+                    knockedTo = (bevonia.self.body.x - (distance*2));
+                    bevonia.vulnerable = false;
+                }
+                bevonia.self.body.velocity.x = -500;
+                if (bevonia.self.body.x <= (knockedTo + distance/2)){
+                    bevonia.self.body.velocity.x = -500;
+                    bevonia.self.body.velocity.y = -200;
+                }
+                else{
+                    bevonia.self.body.velocity.y = -250;
+                    bevonia.self.body.velocity.x = -500;
+                }
+                if (bevonia.self.body.x <= knockedTo){
+                    bevonia.frame = 2;
+                    knockedTo = 0;
+                    knockback = false;
+                }
+                function invincible() {
+                    bevonia.self.body.sprite.alpha = 1;
+                }
+                    bevonia.vulnerable = false;
+                    bevonia.self.body.sprite.alpha = 0.5;
+                    bevonia.invincibilityTimer = game.time.now + bevonia.invincibilityPeriod;                   
+                    game.time.events.add(bevonia.invincibilityPeriod, invincible, this);
+                    
+                }
+            }
+            enemies3[j].manageVulnerability();
+        }
+        
         
         if (game.physics.arcade.collide(bevonia.self, traps3)) {
             bevonia.health -= 1;
